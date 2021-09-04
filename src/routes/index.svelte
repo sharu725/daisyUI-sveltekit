970318f5ec91fe36067f64df58a4a7595b12654e
sharathdt <sharath.dt@gmail.com>
2021-09-04 06:38:02 +0530

    initialize

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<div class="rounded-lg shadow bg-base-200 drawer h-52">
+  <input id="my-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="flex flex-col items-center justify-center drawer-content">
+    <label for="my-drawer" class="btn btn-primary drawer-button"
+      >open menu</label
+    >
+  </div>
+  <div class="drawer-side">
+    <label for="my-drawer" class="drawer-overlay" />
+    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+      <li>
+        <a>Menu Item</a>
+      </li>
+      <li>
+        <a>Menu Item</a>
+      </li>
+    </ul>
+  </div>
+</div>
